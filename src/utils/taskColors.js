44d8bfbe5f7c0b6b47c551bf0455a51d60c1db02
pg147 <xyzprathamesh147@gmail.com>
2025-04-04 17:28:44 +0530

    update: faded bg colors added

--- src/utils/taskColors.js
+++ src/utils/taskColors.js
@@ -1,18 +1,22 @@
 export const taskColors = {
     newTask: {
         main: 'bg-[#0C6EFC]',
+        background: 'bg-[#E9F0FF]',
         stroke: 'text-[#0C6EFC]'
     },
     failedTask: {
         main: 'bg-[#DC3445]',
+        background: 'bg-[#FDEAED]',
         stroke: 'text-[#DC3445]'
     },
     acceptedTask: {
         main: 'bg-[#FFC006]',
+        background: 'bg-[#FFF8EC]',
         stroke: 'text-[#FFC006]'
     },
     completedTask: {
         main: 'bg-[#1EC897]',
+        background: 'bg-[#EDF8F4]',
         stroke: 'text-[#1EC897]'
     }
 }
