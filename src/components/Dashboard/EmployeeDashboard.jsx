1b15471af43a7897c6364f16529804aecd856be9
pg147 <xyzprathamesh147@gmail.com>
2025-04-04 17:34:03 +0530

    update: responsive behaviour improvements

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "../Other/Header.jsx";
-import {taskColors} from "../../utils/taskColors.js";
+import TaskList from "../TaskList/TaskList.jsx";
+import TaskCounts from "./TaskCounts.jsx";
 
 function EmployeeDashboard() {
     // Dashboard task category tiles
@@ -34,17 +35,10 @@ function EmployeeDashboard() {
     const userName = 'Prathmesh';
 
     return (
-        <div className={"w-full flex flex-col space-y-6 max-w-7xl mx-auto"}>
+        <div className={"h-full w-full flex flex-col space-y-6 max-w-7xl mx-auto"}>
             <Header userName={userName}/>
-
-            <div className={"w-full grid grid-cols-2 lg:flex gap-4"}>
-                {taskTiles.map((tiles) => (
-                    <div className={"flex flex-col space-y-2 bg-tile border-[1.5px] border-stroke h-fit w-full rounded-xl shadow-intense p-6"}>
-                        <h1 className={`text-4xl font-bold ${taskColors[tiles.theme.main].stroke}`}>0</h1>
-                        <p className={"text-2xl font-semibold text-heading"}>{tiles.title}</p>
-                    </div>
-                ))}
-            </div>
+            <TaskCounts taskTiles={taskTiles}/>
+            <TaskList/>
         </div>
     );
 }
--- src/components/Dashboard/TaskCounts.jsx
+++ src/components/Dashboard/TaskCounts.jsx
@@ -3,12 +3,12 @@ import {taskColors} from "../../utils/taskColors.js";
 
 function TaskCounts({ taskTiles }) {
     return (
-        <div className={"w-full grid grid-cols-2 lg:flex gap-4"}>
+        <div className={"w-full grid grid-cols-2 lg:flex gap-3 lg:gap-4"}>
             {taskTiles.map((tiles) => (
                 <div
-                    className={"flex flex-col space-y-2 bg-tile border-[1.5px] border-stroke h-fit w-full rounded-xl shadow-intense p-6"}>
-                    <h1 className={`text-4xl font-bold ${taskColors[tiles.theme.main].stroke}`}>0</h1>
-                    <p className={"text-2xl font-semibold text-heading"}>{tiles.title}</p>
+                    className={"flex flex-col space-y-2 bg-tile border-[1.5px] border-stroke h-fit w-full rounded-xl shadow-intense p-4 lg:p-6"}>
+                    <h1 className={`text-2xl lg:text-4xl font-bold ${taskColors[tiles.theme.main].stroke}`}>0</h1>
+                    <p className={"text-lg lg:text-2xl font-semibold text-heading"}>{tiles.title}</p>
                 </div>
             ))}
         </div>
