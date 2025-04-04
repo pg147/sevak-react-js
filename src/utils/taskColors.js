f4e20439a7e0ca0094a8ee21e449d625598370f2
pg147 <xyzprathamesh147@gmail.com>
2025-04-04 16:33:32 +0530

    added: utility file - task colors

---
+++ src/utils/taskColors.js
@@ -0,0 +1,18 @@
+export const taskColors = {
+    newTask: {
+        main: 'bg-[#0C6EFC]',
+        stroke: 'text-[#0C6EFC]'
+    },
+    failedTask: {
+        main: 'bg-[#DC3445]',
+        stroke: 'text-[#DC3445]'
+    },
+    acceptedTask: {
+        main: 'bg-[#FFC006]',
+        stroke: 'text-[#FFC006]'
+    },
+    completedTask: {
+        main: 'bg-[#1EC897]',
+        stroke: 'text-[#1EC897]'
+    }
+}
