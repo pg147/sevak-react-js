734ce9204719bb6edd174a1e7042dd862f7d6e1c
pg147 <xyzprathamesh147@gmail.com>
2025-04-11 21:55:22 +0530

    update: integrated task card component

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -3,7 +3,7 @@ import TaskCards from "./TaskCards.jsx";
 
 function TaskList(props) {
     return (
-        <div id={"taskList"} className={"h-fit overflow-x-auto w-full flex space-x-5 text-nowrap md:rounded-xl"}>
+        <div id={"taskList"} className={"h-fit overflow-x-auto w-full flex space-x-5 flex-nowrap md:rounded-xl"}>
             {Array.from({length: 12}).map((_, index) => {
                     return (
                         <TaskCards index={index} />
